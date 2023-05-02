e05d5ad591fc69aa5d3163d378740dec4ac39738
Yakumaa <maharjanshrish8@gmail.com>
2023-05-02 15:27:26 +0545

    initial commit

---
+++ install_psycopg2.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+import os
+
+python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+target = os.path.join(sys.prefix, 'lib', 'site-packages')
+subprocess.call([python_exe, '-m', 'ensurepip'])
+subprocess.call([python_exe, '-m', 'pip', 'install', '--upgrade', 'pip'])
+subprocess.call([python_exe, '-m', 'pip', 'install',
+                '--upgrade', 'psycopg2-binary', '-t', target])
+
+print('Psycopg2 installed')
